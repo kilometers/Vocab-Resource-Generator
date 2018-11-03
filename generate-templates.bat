ebeaa86b32fd3231871ebcbb158ea10e781e8587
kilometers <mindofmoria@gmail.com>
2018-11-04 01:20:29 +0800

    Generator and Intro clean up
    Generator:
    
    - Copy and rename specific files in one command instead of copying entire folder then deleting
    
    - Renamed to make it more flexible in the future
    
    Intro ppt
    
    - Changed layouts to blank on certain slides

---
+++ generate-templates.bat
@@ -0,0 +1,37 @@
+ECHO OFF
+:identry
+SET /P id=What's the grade-level? (e.g. 5M, 6NFN): 
+IF "%id%" == "6M" GOTO valid
+IF "%id%" == "6I" GOTO valid
+IF "%id%" == "6FN" GOTO valid
+IF "%id%" == "6NFN" GOTO valid
+IF "%id%" == "5M" GOTO valid
+IF "%id%" == "5I" GOTO valid
+IF "%id%" == "5FN" GOTO valid
+
+ECHO Invalid identifier. Here we go again...
+GOTO identry
+
+:valid
+
+:week
+
+SET /P week=What week do you want to generate?: 
+SET vocabpath=%UserProfile%\Documents\Vocabulary
+
+IF EXIST "%vocabpath%\%id%\%week%" ECHO Week %week% already exists. Please manually delete the folder. & GOTO :week
+
+IF NOT EXIST %vocabpath% mkdir %vocabpath%
+IF NOT EXIST %vocabpath%\%id% mkdir %vocabpath%\%id%
+IF NOT EXIST %vocabpath%\%id%\%week% mkdir %vocabpath%\%id%\%week%
+
+XCOPY /q %~dp0.\vocab-quiz-answers.doc "%vocabpath%\%id%\%week%\Vocab Quiz Answers %id% W%week%.doc"*
+XCOPY /q %~dp0.\vocab-intro-handout.doc "%vocabpath%\%id%\%week%\Vocab Intro Handout %id% W%week%.doc"*
+XCOPY /q %~dp0.\vocab-intro.pptm "%vocabpath%\%id%\%week%\Vocab Intro %id% W%week%.pptm"*
+XCOPY /q %~dp0.\words.xlsx "%vocabpath%\%id%\%week%\words.xlsx"*
+MKDIR %vocabpath%\%id%\%week%\images
+
+ECHO Week %week% generated successfully
+GOTO :week
+
+PAUSE
