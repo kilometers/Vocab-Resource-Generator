ae965d275b03c78e345116071fbd06862e91bf1a
kilometers <mindofmoria@gmail.com>
2018-11-12 09:54:44 +0800

    words.xslx renamed
    words.xlsx now reflects the week and level in the name.

--- generate-templates.bat
+++ generate-templates.bat
@@ -18,18 +18,19 @@ GOTO identry
 
 SET /P week=What week do you want to generate?: 
 SET vocabpath=%UserProfile%\Documents\Vocabulary
+SET weekpath=%vocabpath%\%id%\Vocab %id% W%week%
 
-IF EXIST "%vocabpath%\%id%\%week%" ECHO Week %week% already exists. Please manually delete the folder. & GOTO :week
+IF EXIST "%weekpath%" ECHO Week %week% already exists. Please manually delete the folder. & GOTO :week
 
 IF NOT EXIST %vocabpath% mkdir %vocabpath%
 IF NOT EXIST %vocabpath%\%id% mkdir %vocabpath%\%id%
-IF NOT EXIST %vocabpath%\%id%\%week% mkdir %vocabpath%\%id%\%week%
+IF NOT EXIST "%weekpath%" mkdir "%weekpath%"
 
-XCOPY /q %~dp0.\vocab-quiz-answers.doc "%vocabpath%\%id%\%week%\Vocab Quiz Answers %id% W%week%.doc"*
-XCOPY /q %~dp0.\%handout%.doc "%vocabpath%\%id%\%week%\Vocab Intro Handout %id% W%week%.doc"*
-XCOPY /q %~dp0.\vocab-intro.pptm "%vocabpath%\%id%\%week%\Vocab Intro %id% W%week%.pptm"*
-XCOPY /q %~dp0.\words.xlsx "%vocabpath%\%id%\%week%\words.xlsx"*
-MKDIR %vocabpath%\%id%\%week%\images
+XCOPY /q %~dp0.\vocab-quiz-answers.doc "%weekpath%\Vocab Quiz Answers %id% W%week%.doc"*
+XCOPY /q %~dp0.\%handout%.doc "%weekpath%\Vocab Intro Handout %id% W%week%.doc"*
+XCOPY /q %~dp0.\vocab-intro.pptm "%weekpath%\Vocab Intro %id% W%week%.pptm"*
+XCOPY /q %~dp0.\words.xlsx "%weekpath%\words %id% W%week%.xlsx"*
+MKDIR "%weekpath%\images"
 
 ECHO Week %week% generated successfully
 GOTO :identry
